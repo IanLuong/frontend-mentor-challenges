4e4e3a316a2bcc2916c5a8f764130b9dc324148c
Ian <ianluong@hotmail.co.uk>
2023-03-04 21:19:03 +0000

    Add result section design

--- 2-results-summary-component/src/components/ResultsSummary.jsx
+++ 2-results-summary-component/src/components/ResultsSummary.jsx
@@ -2,14 +2,16 @@ const ResultsSummary = () => {
   return (
     <div className="font-hanken">
 
-      <div className="result rounded-b-lg">
-        <h3>Your Result</h3>
-        <div>
-          <h1>76</h1>
-          <p>of 100</p>
+      <div className="flex flex-col p-6 items-center justify-center text-center gap-6 rounded-b-3xl bg-gradient-to-b from-lightSlateBlue to-lightRoyalBlue text-white font-bold">
+        <h3 className="text-lightLavender text-lg">Your Result</h3>
+        <div className="rounded-full bg-gradient-to-b from-violetBlue w-32 h-32 flex flex-col justify-center">
+          <h1 className="text-5xl pb-2">76</h1>
+          <p className="text-lightLavender">of 100</p>
         </div>
-        <h1>Great</h1>
-        <p>You scored higher than 65% of the people who have taken these tests.</p>
+        <div className="flex flex-col items-center gap-2 pb-4">
+          <h1 className="text-2xl">Great</h1>
+          <p className="text-lightLavender w-3/4 font-normal">You scored higher than 65% of the people who have taken these tests.</p>
+        </div>
       </div>
 
       <div className="summary">
