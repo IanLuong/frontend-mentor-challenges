b0061f6969774d2e1fcdb93ac12790ecebcfad70
Ian <ianluong@hotmail.co.uk>
2023-03-05 21:49:54 +0000

    Add complete mobile layout

--- 2-results-summary-component/src/components/CategoryResult.jsx
+++ 2-results-summary-component/src/components/CategoryResult.jsx
@@ -5,7 +5,7 @@ const CategoryResult = ({entry}) => {
     <div className={`flex justify-between p-4 rounded-xl bg-${color} bg-opacity-5`}>
       <div className="flex gap-2">
         <img src={icon} alt={category || "Category"}/>
-        <h4 className={`font-bold text-${color}/60 text-base`}>{category}</h4>
+        <h4 className={`font-bold text-${color} text-base`}>{category}</h4>
       </div>
       <p className="text-base text-darkGrayBlue/60">
         <span className="font-bold text-darkGrayBlue">{score}</span> / 100
--- 2-results-summary-component/src/components/ResultsSummary.jsx
+++ 2-results-summary-component/src/components/ResultsSummary.jsx
@@ -8,23 +8,25 @@ const ResultsSummary = ({data}) => {
         <h3 className="text-lightLavender text-lg">Your Result</h3>
         <div className="rounded-full bg-gradient-to-b from-violetBlue w-32 h-32 flex flex-col justify-center">
           <h1 className="text-5xl pb-2">76</h1>
-          <p className="text-lightLavender">of 100</p>
+          <p className="text-lightLavender/50">of 100</p>
         </div>
         <div className="flex flex-col items-center gap-2 pb-4">
           <h1 className="text-2xl">Great</h1>
-          <p className="text-lightLavender w-3/4">You scored higher than 65% of the people who have taken these tests.</p>
+          <p className="text-lightLavender font-normal w-3/4">You scored higher than 65% of the people who have taken these tests.</p>
         </div>
       </div>
 
       <div className="m-8">
-        <div className="summary text-xl flex flex-col gap-4">
+        <div className="summary text-xl flex flex-col gap-4 mb-8">
           <h3 className="mb-2">Summary</h3>
           {data.map(entry => (
             <CategoryResult key={entry.id} entry={entry}/>)
           )}
         </div>
+      <button className="text-white text-lg bg-darkGrayBlue w-full p-4 rounded-full">Continue</button>
       </div>
-        <button className="text-white bg-darkGrayBlue">Continue</button>
+
+
     </div>
   )
 }
