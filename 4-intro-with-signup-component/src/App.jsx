f401ff90c0e352049e71aa0c73973b003b2762e4
Ian <ianluong@hotmail.co.uk>
2023-03-10 17:01:14 +0000

    Add responsive desktop design

--- 4-intro-with-signup-component/src/App.jsx
+++ 4-intro-with-signup-component/src/App.jsx
@@ -2,7 +2,7 @@ import IntroPage from "./components/IntroPage"
 
 const App = () => {
   return (
-    <div className="App min-h-screen bg-[url('./bg-intro-mobile.png')] md:bg-[url('./bg-intro-desktop.png')] bg-customRed">
+    <div className="App min-h-screen bg-[url('./bg-intro-mobile.png')] md:bg-[url('./bg-intro-desktop.png')] bg-customRed flex items-center justify-center">
       <IntroPage />
     </div>
   )
--- 4-intro-with-signup-component/src/components/IntroPage.jsx
+++ 4-intro-with-signup-component/src/components/IntroPage.jsx
@@ -2,12 +2,12 @@ import SignupForm from "./SignupForm"
 
 const IntroPage = () => {
   return (
-    <main className="p-6 font-poppins">
-      <div className="flex flex-col gap-6 my-14 text-center text-white">
-        <h1 className="font-bold text-3xl">Learn to code by watching others</h1>
+    <main className="md:flex md:justify-center gap-16 p-6 font-poppins items-center">
+      <div className="md:w-1/3 md:text-left flex flex-col gap-6 my-14 text-center text-white">
+        <h1 className="font-bold text-3xl md:text-5xl">Learn to code by watching others</h1>
         <h2 className="text-lg">See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</h2>
       </div>
-      <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-6 md:w-1/3">
         <div className="flex justify-center text-center px-8 py-5 rounded-lg shadow-xl bg-customBlue text-white">
           <p className="w-2/3"><span className="font-semibold">Try it free 7 days</span> then $20/mo. thereafter</p>
         </div>
