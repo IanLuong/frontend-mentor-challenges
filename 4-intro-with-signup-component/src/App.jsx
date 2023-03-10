d6c96c82a6cad90969133a8a45aa6841d6430302
Ian <ianluong@hotmail.co.uk>
2023-03-10 16:27:48 +0000

    Add form styling

--- 4-intro-with-signup-component/src/App.jsx
+++ 4-intro-with-signup-component/src/App.jsx
@@ -2,7 +2,7 @@ import IntroPage from "./components/IntroPage"
 
 const App = () => {
   return (
-    <div className="App h-screen w-screen bg-[url('./bg-intro-mobile.png')] md:bg-[url('./bg-intro-desktop.png')] bg-customRed">
+    <div className="App min-h-screen bg-[url('./bg-intro-mobile.png')] md:bg-[url('./bg-intro-desktop.png')] bg-customRed">
       <IntroPage />
     </div>
   )
--- 4-intro-with-signup-component/src/components/SignupForm.jsx
+++ 4-intro-with-signup-component/src/components/SignupForm.jsx
@@ -20,13 +20,15 @@ const SignupForm = () => {
   }
 
   return (
-    <form className="bg-white rounded-xl shadow-xl p-4" onSubmit={handleSubmit}>
-      <input type='text' placeholder="First Name" name='firstName' value={formDetails.firstName} onChange={handleChange}/>
-      <input type='text' placeholder="Last Name" name='lastName' value={formDetails.lastName} onChange={handleChange}/>
-      <input type='text' placeholder="Email Address" name='email' value={formDetails.email} onChange={handleChange}/>
-      <input type='password' placeholder="Password" name='password' value={formDetails.password} onChange={handleChange}/>
-      <button>CLAIM YOUR FREE TRIAL</button>
-      <p>By clicking the button, you are agreeing to our <span className="text-customRed hover:underline hover:cursor-pointer">Terms and Services</span></p>
+    <form className="bg-white rounded-xl shadow-xl p-6 flex flex-col gap-5 font-semibold" onSubmit={handleSubmit}>
+      <div className="flex flex-col gap-3">
+        <input className="border border-grayBlue rounded-md p-4 w-full" type='text' placeholder="First Name" name='firstName' value={formDetails.firstName} onChange={handleChange}/>
+        <input className="border border-grayBlue rounded-md p-4 w-full" type='text' placeholder="Last Name" name='lastName' value={formDetails.lastName} onChange={handleChange}/>
+        <input className="border border-grayBlue rounded-md p-4 w-full" type='text' placeholder="Email Address" name='email' value={formDetails.email} onChange={handleChange}/>
+        <input className="border border-grayBlue rounded-md p-4 w-full" type='password' placeholder="Password" name='password' value={formDetails.password} onChange={handleChange}/>
+      </div>
+      <button className="p-5 bg-customGreen rounded-md shadow-xl text-white text-base tracking-wider">CLAIM YOUR FREE TRIAL</button>
+      <p className="text-center w-4/5 self-center text-grayBlue text-xs">By clicking the button, you are agreeing to our <span className="text-customRed hover:underline hover:cursor-pointer">Terms and Services</span></p>
     </form>
   )
 }
