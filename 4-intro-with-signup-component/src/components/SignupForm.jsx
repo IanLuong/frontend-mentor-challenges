9418632229583c1a7c83f25f10596ae61f0deb61
Ian <ianluongcodes@gmail.com>
2023-03-11 02:34:38 +0000

    Fix error message styling

--- 4-intro-with-signup-component/src/components/SignupForm.jsx
+++ 4-intro-with-signup-component/src/components/SignupForm.jsx
@@ -1,4 +1,6 @@
-import { useEffect, useState } from "react"
+import {useState} from "react"
+
+const isValidEmail = (email) => email.match(/\w+@\w+(.\w+)+/)
 
 const SignupForm = () => {
 
@@ -16,33 +18,37 @@ const SignupForm = () => {
     password: false,
   })
 
-// .match(/\w+@\w+(.\w+)+/)
-
-  useEffect(() => {
-
-  }, [formDetails])
-
   const handleChange = (event) => {
     const {name, value} = event.target
     setFormDetails(prevForm => ({...prevForm, [name]: value}))
+
+    let error
+
+    if(['firstName', 'lastName', 'password'].indexOf(name) > -1) {
+      error = value ? false : true
+    }else if (name === 'email') {
+      error = isValidEmail(formDetails.email) ? false : true
+    } else {
+      error = false
+    }
+    setErrors({...errors, [name]: error})
   }
 
-  const handleSubmit = (target) => {
+  const handleSubmit = (event) => {
     event.preventDefault()
-    console.log(target)
     console.log('Submitted')
   }
 
   return (
     <form className="bg-white rounded-xl shadow-xl p-6 flex flex-col gap-5 font-semibold" onSubmit={handleSubmit}>
       <div className="flex flex-col gap-3">
-        <input className={`border ${formDetails.firstName ? 'border-grayBlue' : 'border-customRed border-2'} rounded-md p-4 w-full`} type='text' placeholder="First Name" name='firstName' value={formDetails.firstName} onChange={handleChange} />
+        <input className={`border ${!errors.firstName ? 'border-grayBlue' : 'border-customRed border-2 text-customRed focus:outline-none'} rounded-md p-4 w-full`} type='text' placeholder="First Name" name='firstName' value={formDetails.firstName} onChange={handleChange} />
         {errors.firstName && <p className="text-right italic text-customRed text-[.7rem]">First Name cannot be empty</p>}
-        <input className={`border ${formDetails.lastName ? 'border-grayBlue' : 'border-customRed border-2'} rounded-md p-4 w-full`} type='text' placeholder="Last Name" name='lastName' value={formDetails.lastName} onChange={handleChange}/>
+        <input className={`border ${!errors.lastName ? 'border-grayBlue' : 'border-customRed border-2 text-customRed focus:outline-none'} rounded-md p-4 w-full`} type='text' placeholder="Last Name" name='lastName' value={formDetails.lastName} onChange={handleChange}/>
         {errors.lastName && <p className="text-right italic text-customRed text-[.7rem]">Last Name cannot be empty</p>}
-        <input className={`border ${formDetails.email.match(/\w+@\w+(.\w+)+/) ? 'border-grayBlue' : 'border-customRed border-2'} rounded-md p-4 w-full`} type='email' placeholder="Email Address" name='email' value={formDetails.email} onChange={handleChange}/>
+        <input className={`border ${!errors.email ? 'border-grayBlue' : 'border-customRed border-2 text-customRed focus:outline-none'} rounded-md p-4 w-full`} type='email' placeholder="Email Address" name='email' value={formDetails.email} onChange={handleChange}/>
         {errors.email && <p className="text-right italic text-customRed text-[.7rem]">Looks like this is not an email</p>}
-        <input className={`border ${formDetails.password ? 'border-grayBlue' : 'border-customRed border-2'} rounded-md p-4 w-full`} type='password' placeholder="Password" name='password' value={formDetails.password} onChange={handleChange}/>
+        <input className={`border ${!errors.password ? 'border-grayBlue' : 'border-customRed border-2 text-customRed focus:outline-none'} rounded-md p-4 w-full`} type='password' placeholder="Password" name='password' value={formDetails.password} onChange={handleChange}/>
         {errors.password && <p className="text-right italic text-customRed text-[.7rem]">Password cannot be empty</p>}
       </div>
       <button disabled={!Object.values(formDetails).every(val => val)} className="p-5 bg-customGreen rounded-md shadow-xl text-white text-base tracking-wider hover:cursor-pointer enabled:hover:bg-darkerGreen disabled:cursor-default disabled:opacity-70">CLAIM YOUR FREE TRIAL</button>
