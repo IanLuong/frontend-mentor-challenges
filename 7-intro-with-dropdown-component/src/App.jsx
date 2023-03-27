5076f0c2efd02b0b1ba1e3acbed7f169da5163d6
Ian <ianluong@hotmail.co.uk>
2023-03-27 16:30:02 +0100

    Add sidebar base

--- 7-intro-with-dropdown-component/src/App.jsx
+++ 7-intro-with-dropdown-component/src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
 
 function App() {
   return (
-    <div className="App font-epilogue">
+    <div className="App font-epilogue relative">
+      <Sidebar />
       <Navbar />
       <Hero />
     </div>
---
+++ 7-intro-with-dropdown-component/src/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+const Sidebar = () => {
+  return (
+    <section className="flex fixed w-screen h-screen">
+      <div className="bg-black opacity-80 w-1/3"></div>
+      <div className="bg-almostWhite w-2/3"></div>
+    </section>
+  )
+}
+
+export default Sidebar
