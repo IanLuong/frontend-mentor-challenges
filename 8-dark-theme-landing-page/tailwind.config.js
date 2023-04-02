c6a906b29c0ee1bdb986368ab8e3d3fa06f84715
Ian <ianluongcodes@gmail.com>
2023-04-02 23:51:05 +0100

    Add custom tailwind themes

--- 8-dark-theme-landing-page/tailwind.config.js
+++ 8-dark-theme-landing-page/tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accentBackground: '#1C2431',
+        mainBackground: '#181F2A',
+        footerBackground: '#0B1523',
+        testimonialsBackground: '#202A3C',
+
+        gradientCyan: '#65E2D9',
+        gradientBlue: '#339ECC',
+        errorRed: '#FF4242',
+      },
+      fontFamily: {
+        raleway: ['Raleway', 'sans-serif'],
+        openSans: ['Open Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
